1a02f1e8dcf39bb12ab326389b065113fd5ff177
saitoyoshi <mitti180@gmail.com>
2023-05-01 11:18:15 +0900

    ヴァリデーションを追加

--- src/login.php
+++ src/login.php
@@ -2,18 +2,45 @@
 
 require_once __DIR__ . '/classes/User.php';
 session_start();
+require_once __DIR__ . '/libs/utils.php';
+// todoすでにログインしていたら一覧ページにとばす
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $user = new User($password, $email);
-    if ($user->login()) {
-        session_regenerate_id();
-        $_SESSION['user'] = $user;
-        header('Location: list.php');
-        exit;
+    // ヴァリデーション ここはしっかりしません
+    if (isset($_SESSION['errors'])) {
+        unset($_SESSION['errors']);
+    }
+    if (isset($_SESSION['msg'])) {
+        unset($_SESSION['msg']);
+    }
+    $errors = [];
+    if (!filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL)) {
+        $errors['email'] = 'メールアドレスの形式が正しくありません';
+    }
+    if (strlen($_POST['password']) < 8) {
+        $errors['password'] = 'パスワードは最低8文字でなければなりません';
+    }
+    if (count($errors) > 0) {
+        $_SESSION['errors'] = $errors;
+    } else {
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $user = new User($password, $email);
+        if ($user->login()) {
+            session_regenerate_id();
+            $_SESSION['user'] = $user;
+            if ($_SESSION['errors']) {
+                unset($_SESSION['errors']);
+            }
+            if ($_SESSION['msg']) {
+                unset($_SESSION['msg']);
+            }
+            header('Location: list.php');
+            exit;
+        } else {
+            $_SESSION['msg'] = 'メールアドレスまたはパスワードが異なります';
+        }
     }
 }
-
 
 
 $title = 'login page';
--- src/views/login.php
+++ src/views/login.php
@@ -1,14 +1,24 @@
 <div class="container py-4">
     <h4 class="mb-3">ログイン画面</h4>
+    <?php if (isset($_SESSION['msg'])): ?>
+                <p class="text-danger mt-3"><?php echo h($_SESSION['msg']) ?></p>
+            <?php endif; ?>
     <form action="" method="post">
+
         <div class="mb-3">
-            <label for="email" class="form-label">メールアドレス</label>
-            <input type="email" id="email" name="email" class="form-control">
+            <label for="msg" class="form-label">メールアドレス</label>
+            <input type="email" id="email" name="email" class="form-control" value="<?php if (isset($_POST['email'])): ?><?php echo h($_POST['email']) ?><?php endif; ?>">
+            <?php if (isset($_SESSION['errors']['email'])): ?>
+                <p class="text-danger mt-3"><?php echo h($_SESSION['errors']['email']) ?></p>
+            <?php endif; ?>
         </div>
         <div class="mb-3">
             <label for="password" class="form-label">パスワード</label>
             <input type="password" name="password" id="password" class="form-control">
+            <?php if (isset($_SESSION['errors']['password'])): ?>
+                <p class="text-danger mt-3"><?php echo h($_SESSION['errors']['password']) ?></p>
+            <?php endif; ?>
         </div>
-        <button class="btn btn-primary">ログイン</button>
+        <button class="btn btn-primary pt-3">ログイン</button>
     </form>
 </div>

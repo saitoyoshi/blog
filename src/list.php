4e779041a20770181aa32e2cd3f820991043ceb9
saitoyoshi <mitti180@gmail.com>
2023-05-01 11:50:46 +0900

    ログインしていない場合にログインページに遷移させる

--- src/list.php
+++ src/list.php
@@ -1,9 +1,8 @@
 <?php
 
+require_once __DIR__ . '/auth.php';
 require_once __DIR__ . '/classes/User.php';
-session_start();
-
-
+var_dump($_SESSION);
 $title = 'list page';
 $content = __DIR__ . '/views/list.php';
 include __DIR__ . '/views/layout.php';

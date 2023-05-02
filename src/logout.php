5befa44ac24b161443a8386da529a994ab38338b
saitoyoshi <mitti180@gmail.com>
2023-05-02 10:55:31 +0900

    クライアントのクッキーを削除

--- src/logout.php
+++ src/logout.php
@@ -4,6 +4,9 @@ session_start();
 if (isset($_SESSION['user'])) {
     $_SESSION = [];
     session_destroy();
+    // クライアント側のセッションIDクッキーを削除
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
 }
 
 header('Location: login.php');

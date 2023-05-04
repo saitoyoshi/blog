d4f2f57d6dbeb62e4c18834f515e7e41931f3adf
saitoyoshi <mitti180@gmail.com>
2023-05-04 10:13:44 +0900

    HTMLにPHPを埋め込む書式に変更

--- src/views/content.php
+++ src/views/content.php
@@ -2,14 +2,12 @@
     <h4 class="mb-3">記事画面</h4>
     <a href="../list.php">戻る</a>
     <a href="../logout.php">logout</a>
-    <?php
-    if ($post) {
-        // 投稿の表示
-        echo '<h2>' . h($post->getTitle()) . '</h2>';
-        echo '<p>' . nl2br(h($post->getContent())) . '</p>';
-        echo '<p>Posted on: ' . h($post->getCreatedAt()) . '</p>';
-    }
-    ?>
+    <?php if ($post) : ?>
+        <?php // 投稿の表示 ?>
+        <h2><?php echo h($post->getTitle()) ?></h2>
+        <p><?php echo nl2br(h($post->getContent())) ?></p>
+        <p>Posted on: <?php echo h($post->getCreatedAt()) ?></p>
+    <?php endif; ?>
     <form action="" method="post">
     </form>
 </div>

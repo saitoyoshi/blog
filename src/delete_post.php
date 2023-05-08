23a67bdb046e5dc9ca58ba72f4ad9d64298eca02
saitoyoshi <mitti180@gmail.com>
2023-05-08 12:40:03 +0900

    子行が残っていると親行が削除できないのを修正

--- src/delete_post.php
+++ src/delete_post.php
@@ -13,8 +13,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $postId = $_POST['post_id'];
         $sql = "SELECT * FROM posts WHERE id = ?";
         $result = db($sql, $postId);
-
         if ($result[0] && $result[0]['user_id'] == $_SESSION['user']->getId()) {
+            // 子行がのこっていると削除できないので。
+            $sql = "DELETE FROM post_tags WHERE post_id = ?";
+            db($sql, $postId);
+
             $sql = "DELETE FROM posts WHERE id = ?";
             db($sql, $postId);
         }
